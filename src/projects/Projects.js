bfe86d8710bea9c411cf7872ac9983ba03d8f57f
Clayton <Claytonhart123@yahoo.com>
2018-09-26 23:13:27 -0700

    added cryptocurrency project

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -6,6 +6,7 @@ import playlistconverterimage from "../images/playlist-converter-small.jpg";
 import landingimage from "../images/landing-page-small.jpg";
 import booksearchimage from "../images/book-search-small.jpg";
 import cryptogramimage from "../images/cryptogram-small.jpg";
+import cryptocurrencyimage from "../images/cryptocurrency-small.jpg";
 
 import ProjectCard from "./projectcard/ProjectCard";
 
@@ -25,6 +26,18 @@ const Projects = () => {
   };
 
   const p2 = {
+    title: "Cryptocurrency Dashboard (WIP)",
+    tech: "React, HTML5, CSS3, Highcharts",
+    description: `Cryptocurrency Dashboard is a simple example of a single page dashboard 
+    which allows users to search a database of over 3,000 cryptocurrencies and see their 
+    price history and most recent price snapshots. Currently a work in progress with most 
+    of the functionality to be implemented.`,
+    image: cryptocurrencyimage,
+    webLink: "https://claytonhart.github.io/cryptocurrency/",
+    gitLink: "https://github.com/Claytonhart/cryptocurrency/"
+  };
+
+  const p3 = {
     title: "Landing Page",
     tech: "React, HTML5, CSS3, SASS, SCSS",
     description: `This project was an anonymized recreation of the avenueliving.ca home page. It was my first project using Sass, with a focus on clean components and reusable CSS.`,
@@ -33,17 +46,17 @@ const Projects = () => {
     gitLink: "https://github.com/Claytonhart/sass-test"
   };
 
-  const p3 = {
-    title: "Book Search",
-    tech: "React, React Router, HTML5, CSS3",
-    description: `Book Search pulls from the Google Books api to allow users to search for
-    any book by title. Unfortunately, the api is rather limited and does not allow much
-    customization of search parameters, however, the intent of the project was to practice
-    flexbox and responsive design in the React ecosystem.`,
-    image: booksearchimage,
-    webLink: "https://claytonhart.github.io/book-search/",
-    gitLink: "https://github.com/Claytonhart/book-search"
-  };
+  // const p3 = {
+  //   title: "Book Search",
+  //   tech: "React, React Router, HTML5, CSS3",
+  //   description: `Book Search pulls from the Google Books api to allow users to search for
+  //   any book by title. Unfortunately, the api is rather limited and does not allow much
+  //   customization of search parameters, however, the intent of the project was to practice
+  //   flexbox and responsive design in the React ecosystem.`,
+  //   image: booksearchimage,
+  //   webLink: "https://claytonhart.github.io/book-search/",
+  //   gitLink: "https://github.com/Claytonhart/book-search"
+  // };
 
   const p4 = {
     title: "Cryptograms",
