ffca3562b2b6203e18fdab05a2a4552e23b93951
Clayton Hart <claytonhart123@yahoo.com>
2019-10-15 15:28:47 -0700

    updated playlist converter link & image

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./Projects.css";
 
-import playlistconverterimage from "../images/playlist-converter-small.jpg";
+import playlistconverterimage from "../images/music-playlist-converter-small.jpg";
 import landingimage from "../images/landing-page-small.jpg";
 import booksearchimage from "../images/book-search-small.jpg";
 import cryptogramimage from "../images/cryptogram-small.jpg";
@@ -13,7 +13,7 @@ import ProjectCard from "./projectcard/ProjectCard";
 const Projects = () => {
   const p1 = {
     title: "Playlist Converter",
-    tech: "React, Redux, React Router, Redux Thunk, HTML5, CSS3",
+    tech: "React, React Router, HTML5, SASS, CSS3",
     description: `This Playlist Converter was a project initially designed to convert my
     music library from Youtube to Spotify. It grew to accommodate four platforms which
     each have a public api with client side authentication and authorization. I'd like
@@ -21,8 +21,8 @@ const Projects = () => {
     each of these platforms seemed to either only allow server side authentication, or
     did not have a publicly accessible api at the moment (Soundcloud).`,
     image: playlistconverterimage,
-    webLink: "https://claytonhart.github.io/playlist-converter/",
-    gitLink: "https://github.com/Claytonhart/playlist-converter"
+    webLink: "https://claytonhart.github.io/music-playlist-converter/",
+    gitLink: "https://github.com/Claytonhart/music-playlist-converter"
   };
 
   const p2 = {
@@ -58,16 +58,16 @@ const Projects = () => {
   //   gitLink: "https://github.com/Claytonhart/book-search"
   // };
 
-  const p4 = {
-    title: "Cryptograms",
-    tech: "React, HTML5, CSS3",
-    description: `This simple Cryptogram creator encrypts a random short quote with a 
-    substitution cipher. Each letter is linked to all instances of the same letter, so 
-    users do not have to search for each instance of a letter.`,
-    image: cryptogramimage,
-    webLink: "https://claytonhart.github.io/cryptograms/",
-    gitLink: "https://github.com/Claytonhart/cryptograms/"
-  };
+  // const p4 = {
+  //   title: "Cryptograms",
+  //   tech: "React, HTML5, CSS3",
+  //   description: `This simple Cryptogram creator encrypts a random short quote with a
+  //   substitution cipher. Each letter is linked to all instances of the same letter, so
+  //   users do not have to search for each instance of a letter.`,
+  //   image: cryptogramimage,
+  //   webLink: "https://claytonhart.github.io/cryptograms/",
+  //   gitLink: "https://github.com/Claytonhart/cryptograms/"
+  // };
 
   return (
     <section className="projects" id="projects">
@@ -75,7 +75,7 @@ const Projects = () => {
       <ProjectCard project={p1} />
       <ProjectCard project={p2} />
       <ProjectCard project={p3} />
-      <ProjectCard project={p4} />
+      {/* <ProjectCard project={p4} /> */}
     </section>
   );
 };
