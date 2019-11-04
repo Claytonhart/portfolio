939f18a5a8e5d31f0c072e66e5c1400b22ab9fa2
Clayton Hart <claytonhart123@yahoo.com>
2019-11-04 07:02:08 -0800

    added roadmap project

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React from 'react';
 
-import "./Projects.css";
+import './Projects.css';
 
-import playlistconverterimage from "../images/music-playlist-converter-small.jpg";
-import landingimage from "../images/landing-page-small.jpg";
-import booksearchimage from "../images/book-search-small.jpg";
-import cryptogramimage from "../images/cryptogram-small.jpg";
-import cryptocurrencyimage from "../images/cryptocurrency-small.jpg";
+import roadmapimage from '../images/roadmap-small.jpg';
+import playlistconverterimage from '../images/music-playlist-converter-small.jpg';
+import landingimage from '../images/landing-page-small.jpg';
+import booksearchimage from '../images/book-search-small.jpg';
+import cryptogramimage from '../images/cryptogram-small.jpg';
+import cryptocurrencyimage from '../images/cryptocurrency-small.jpg';
 
-import ProjectCard from "./projectcard/ProjectCard";
+import ProjectCard from './projectcard/ProjectCard';
 
 const Projects = () => {
   const p1 = {
-    title: "Playlist Converter",
-    tech: "React, React Router, HTML5, SASS, CSS3",
+    title: 'Roadmap',
+    tech:
+      'React, Redux, Node.js, Express, MongoDB, Styled Components, React Router, HTML5, CSS3',
+    description: `Roadmap is a full stack project management tool inspired by Trello, Jira, Asana, and more. Users can sign up, create projects with tasks, add other users to their projects, and delete those projects.`,
+    image: roadmapimage,
+    webLink: 'https://devroadmap.herokuapp.com/',
+    gitLink: 'https://github.com/Claytonhart/roadmap'
+  };
+
+  const p2 = {
+    title: 'Playlist Converter',
+    tech: 'React, React Router, HTML5, SASS, CSS3',
     description: `This Playlist Converter was a project initially designed to convert my
     music library from Youtube to Spotify. It grew to accommodate four platforms which
     each have a public api with client side authentication and authorization. I'd like
@@ -21,29 +32,28 @@ const Projects = () => {
     each of these platforms seemed to either only allow server side authentication, or
     did not have a publicly accessible api at the moment (Soundcloud).`,
     image: playlistconverterimage,
-    webLink: "https://claytonhart.github.io/music-playlist-converter/",
-    gitLink: "https://github.com/Claytonhart/music-playlist-converter"
-  };
-
-  const p2 = {
-    title: "Cryptocurrency Dashboard",
-    tech: "React, HTML5, CSS3, SASS, Highcharts",
-    description: `Cryptocurrency Dashboard is a simple example of a single page dashboard 
-    which allows users to search a database of over 3,000 cryptocurrencies and see their 
-    price history and most recent price snapshots. Currently a work in progress with most 
-    of the functionality to be implemented.`,
-    image: cryptocurrencyimage,
-    webLink: "https://claytonhart.github.io/cryptocurrency-dashboard/",
-    gitLink: "https://github.com/Claytonhart/cryptocurrency-dashboard/"
+    webLink: 'https://claytonhart.github.io/music-playlist-converter/',
+    gitLink: 'https://github.com/Claytonhart/music-playlist-converter'
   };
 
   const p3 = {
-    title: "Simple Landing Page",
-    tech: "React, HTML5, CSS3, SASS, SCSS",
+    title: 'Cryptocurrency Dashboard',
+    tech: 'React, HTML5, CSS3, SASS, Highcharts',
+    description: `Cryptocurrency Dashboard is an example of a single page dashboard 
+    which allows users to search a database of over 3,000 cryptocurrencies and see their 
+    price history and most recent price snapshots.`,
+    image: cryptocurrencyimage,
+    webLink: 'https://claytonhart.github.io/cryptocurrency-dashboard/',
+    gitLink: 'https://github.com/Claytonhart/cryptocurrency-dashboard/'
+  };
+
+  const p4 = {
+    title: 'Simple Landing Page',
+    tech: 'React, HTML5, CSS3, SASS, SCSS',
     description: `This project was an anonymized recreation of the avenueliving.ca home page. It was my first project using Sass, with a focus on clean components and reusable CSS.`,
     image: landingimage,
-    webLink: "https://claytonhart.github.io/simple-landing-page/",
-    gitLink: "https://github.com/Claytonhart/simple-landing-page/"
+    webLink: 'https://claytonhart.github.io/simple-landing-page/',
+    gitLink: 'https://github.com/Claytonhart/simple-landing-page/'
   };
 
   // const p3 = {
@@ -70,12 +80,12 @@ const Projects = () => {
   // };
 
   return (
-    <section className="projects" id="projects">
-      <h1 className="projects-title">Projects</h1>
+    <section className='projects' id='projects'>
+      <h1 className='projects-title'>Projects</h1>
       <ProjectCard project={p1} />
       <ProjectCard project={p2} />
       <ProjectCard project={p3} />
-      {/* <ProjectCard project={p4} /> */}
+      <ProjectCard project={p4} />
     </section>
   );
 };
